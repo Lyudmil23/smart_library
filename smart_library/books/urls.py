f72065fb9c8932d8ca5427cd69273cb39783c36f
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-03-17 17:28:45 +0200

    Added another Book view and setting up the html and css

--- smart_library/books/urls.py
+++ smart_library/books/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from smart_library.books.views import BooksView
+from smart_library.books.views import BooksView, BookDetailsView
 
 urlpatterns = [
     path('', BooksView.as_view(), name='books'),
+    path('book/<int:pk>/', include([
+        path('', BookDetailsView.as_view(), name='book details'),
+    ]))
 
 ]
--- smart_library/books/views.py
+++ smart_library/books/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, TemplateView
 
 from smart_library.books.models import Book
 
@@ -9,3 +9,16 @@ class BooksView(ListView):
     template_name = 'books/books.html'
     context_object_name = 'books'
     paginate_by = 6
+
+
+class BookDetailsView(TemplateView):
+    template_name = 'books/book.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book = get_object_or_404(Book, pk=kwargs['pk'])
+        context.update({
+            'book': book,
+        })
+
+        return context
