19e22b5f5aeb138e158be18da2bd6f8696623eaf
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-03-18 16:05:52 +0200

    Added another view for the Book and corresponding html and css.

--- smart_library/books/urls.py
+++ smart_library/books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from smart_library.books.views import BooksView, BookDetailsView
+from smart_library.books.views import BooksView, BookDetailsView, rent_book
 
 urlpatterns = [
     path('', BooksView.as_view(), name='books'),
     path('book/<int:pk>/', include([
         path('', BookDetailsView.as_view(), name='book details'),
+        path('rent/', rent_book, name='rent book')
     ]))
 
 ]
--- smart_library/books/views.py
+++ smart_library/books/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, TemplateView
 
-from smart_library.books.models import Book
+from smart_library.books.forms import RentBookForm
+from smart_library.books.models import Book, RentBook
 
 
 class BooksView(ListView):
@@ -22,3 +25,31 @@ class BookDetailsView(TemplateView):
         })
 
         return context
+
+
+@login_required
+def rent_book(request, pk, *args, **kwargs):
+    try:
+        book = Book.objects.get(id=pk)
+    except Book.DoesNotExist:
+        return render(request, "404.html")
+
+    form = RentBookForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            # Checking if the user has already rented this book
+            if RentBook.objects.filter(user=request.user, book=book).exists():
+                messages.error(request, "You have already rented this book.")
+            else:
+                rent_book = form.save(commit=False)
+                rent_book.user = request.user
+                rent_book.book = book
+                rent_book.save()
+                return redirect('home')
+
+    context = {
+        'book': book,
+        'form': form,
+    }
+    return render(request, "books/rent_book.html", context)
