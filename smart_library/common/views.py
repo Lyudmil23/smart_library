a4e3af3e1159e0d49f74c151c2567d7ac3d9fb74
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-03-20 13:09:59 +0200

    Added another view and corresponding html and css

--- smart_library/common/views.py
+++ smart_library/common/views.py
@@ -1,5 +1,20 @@
+from django.db.models import Avg
+from django.db.models.functions import Coalesce
 from django.shortcuts import render
+
+from smart_library.books.models import Book
 
 
 def home(request):
-    return render(request, 'common/home.html')
+    books_average_rate = Book.objects.prefetch_related("reviews").annotate(
+        avg_rate=Coalesce(Avg('reviews__rate'), 0.0)
+    )
+
+    # Ordering books by average rating in descending order
+    top_books = books_average_rate.order_by("-avg_rate")[:3]
+
+    context = {
+        'top_books': top_books,
+    }
+
+    return render(request, 'common/home.html', context)
